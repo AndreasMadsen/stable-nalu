7f70a25d5123ff524d220d32234d4380bfc65367
Andreas Madsen <amwebdk@gmail.com>
2019-03-28 14:07:54 +0100

    document nac_weight

--- stable_nalu/functional/nac_weight.py
+++ stable_nalu/functional/nac_weight.py
@@ -2,6 +2,11 @@
 import torch
 
 class NACWeight(torch.autograd.Function):
+    """Implements the NAC weight operator
+
+    w = tanh(\hat{w}) * sigmoid(\hat{m})
+    """
+
     @staticmethod
     def forward(ctx, w_hat, m_hat):
         tanh_w_hat = torch.tanh(w_hat)
@@ -19,7 +24,13 @@ class NACWeight(torch.autograd.Function):
         )
 
 class NACWeightRescaled(torch.autograd.Function):
-    @staticmethod
+    """Implements the NAC weight operator but with a hard gradient for \hat{m}
+
+    w = tanh(\hat{w}) * sigmoid(\hat{m})
+    dL/d\hat{m} = (dL/dw) (dw/d\hat{m})
+                = (dL/dw) * 0.1 * sign(\hat{w}) * sigmoid(\hat{m}) * (1 - sigmoid(\hat{m}))
+    """
+        @staticmethod
     def forward(ctx, w_hat, m_hat):
         tanh_w_hat = torch.tanh(w_hat)
         sigmoid_m_hat = torch.sigmoid(m_hat)
