0445100bb7010a7cb96499fc8a51c6a7e1db89f0
Andreas Madsen <amwebdk@gmail.com>
2019-05-14 09:37:08 +0200

    move LFS to compute cluster

--- lfs_batch_jobs/sequential_mnist_prod.sh
+++ lfs_batch_jobs/sequential_mnist_prod.sh
@@ -3,21 +3,21 @@ experiment_name='sequential_mnist_prod'
 
 for seed in {0..10}
 do
-    bsub -q gpuvoltasxm2i -n 2 -gpu "num=1:mode=exclusive_process" -W 24:00 -J ${experiment_name} -o /work3/$USER/logs/${experiment_name} -e /work3/$USER/logs/${experiment_name} -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
+    bsub -q compute -n 16 -W 48:00 -J ${experiment_name} -o /work3/$USER/logs/${experiment_name} -e /work3/$USER/logs/${experiment_name} -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
         experiments/sequential_mnist.py \
         --operation prod --layer-type NAC --nac-mul normal \
         --interpolation-length 3 --extrapolation-short-length 6 --extrapolation-long-length 9 \
         --seed ${seed} --max-epochs 10000 --verbose \
         --name-prefix ${experiment_name} --remove-existing-data
 
-    bsub -q gpuvoltasxm2i -n 2 -gpu "num=1:mode=exclusive_process" -W 24:00 -J ${experiment_name} -o /work3/$USER/logs/${experiment_name} -e /work3/$USER/logs/${experiment_name} -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
+    bsub -q compute -n 16 -W 48:00 -J ${experiment_name} -o /work3/$USER/logs/${experiment_name} -e /work3/$USER/logs/${experiment_name} -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
         experiments/sequential_mnist.py \
         --operation prod --layer-type NALU \
         --interpolation-length 3 --extrapolation-short-length 6 --extrapolation-long-length 9 \
         --seed ${seed} --max-epochs 10000 --verbose \
         --name-prefix ${experiment_name} --remove-existing-data
 
-    bsub -q gpuvoltasxm2i -n 2 -gpu "num=1:mode=exclusive_process" -W 24:00 -J ${experiment_name} -o /work3/$USER/logs/${experiment_name} -e /work3/$USER/logs/${experiment_name} -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
+    bsub -q compute -n 16 -W 48:00 -J ${experiment_name} -o /work3/$USER/logs/${experiment_name} -e /work3/$USER/logs/${experiment_name} -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
         experiments/sequential_mnist.py \
         --operation prod --layer-type ReRegualizedLinearNAC --nac-mul mnac \
         --interpolation-length 3 --extrapolation-short-length 6 --extrapolation-long-length 9 \
