4431532615e8784f6ea545c5ad7077d677f66c46
Andreas Madsen <amwebdk@gmail.com>
2019-05-15 13:23:52 +0200

    fix MNIST LFS batch scripts

--- lfs_batch_jobs/sequential_mnist_prod.sh
+++ lfs_batch_jobs/sequential_mnist_prod.sh
@@ -3,23 +3,30 @@ experiment_name='sequential_mnist_prod'
 
 for seed in {0..10}
 do
-    bsub -q compute -n 16 -W 48:00 -J ${experiment_name} -o /work3/$USER/logs/${experiment_name} -e /work3/$USER/logs/${experiment_name} -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
+    bsub -q compute -n 16 -W 72:00 -J ${experiment_name} -o /work3/$USER/logs/${experiment_name}/ -e /work3/$USER/logs/${experiment_name}/ -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
         experiments/sequential_mnist.py \
         --operation prod --layer-type NAC --nac-mul normal \
         --interpolation-length 3 --extrapolation-short-length 6 --extrapolation-long-length 9 \
         --seed ${seed} --max-epochs 10000 --verbose \
         --name-prefix ${experiment_name} --remove-existing-data
 
-    bsub -q compute -n 16 -W 48:00 -J ${experiment_name} -o /work3/$USER/logs/${experiment_name} -e /work3/$USER/logs/${experiment_name} -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
+    bsub -q compute -n 16 -W 72:00 -J ${experiment_name} -o /work3/$USER/logs/${experiment_name}/ -e /work3/$USER/logs/${experiment_name}/ -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
         experiments/sequential_mnist.py \
         --operation prod --layer-type NALU \
         --interpolation-length 3 --extrapolation-short-length 6 --extrapolation-long-length 9 \
         --seed ${seed} --max-epochs 10000 --verbose \
         --name-prefix ${experiment_name} --remove-existing-data
 
-    bsub -q compute -n 16 -W 48:00 -J ${experiment_name} -o /work3/$USER/logs/${experiment_name} -e /work3/$USER/logs/${experiment_name} -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
+    bsub -q compute -n 16 -W 72:00 -J ${experiment_name} -o /work3/$USER/logs/${experiment_name}/ -e /work3/$USER/logs/${experiment_name}/ -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
         experiments/sequential_mnist.py \
         --operation prod --layer-type ReRegualizedLinearNAC --nac-mul mnac \
+        --interpolation-length 3 --extrapolation-short-length 6 --extrapolation-long-length 9 \
+        --seed ${seed} --max-epochs 10000 --verbose \
+        --name-prefix ${experiment_name} --remove-existing-data
+
+    bsub -q compute -n 16 -W 72:00 -J ${experiment_name} -o /work3/$USER/logs/${experiment_name}/ -e /work3/$USER/logs/${experiment_name}/ -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
+        experiments/sequential_mnist.py \
+        --operation prod --layer-type LSTM \
         --interpolation-length 3 --extrapolation-short-length 6 --extrapolation-long-length 9 \
         --seed ${seed} --max-epochs 10000 --verbose \
         --name-prefix ${experiment_name} --remove-existing-data
--- lfs_batch_jobs/sequential_mnist_sum.sh
+++ lfs_batch_jobs/sequential_mnist_sum.sh
@@ -23,4 +23,11 @@ do
         --interpolation-length 10 --extrapolation-short-length 100 --extrapolation-long-length 1000 \
         --seed ${seed} --max-epochs 10000 --verbose \
         --name-prefix ${experiment_name} --remove-existing-data
+
+    bsub -q gpuv100 -n 2 -gpu "num=1:mode=exclusive_process" -W 24:00 -J ${experiment_name} -o /work3/$USER/logs/${experiment_name}/ -e /work3/$USER/logs/${experiment_name}/ -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
+        experiments/sequential_mnist.py \
+        --operation sum --layer-type LSTM \
+        --interpolation-length 10 --extrapolation-short-length 100 --extrapolation-long-length 1000 \
+        --seed ${seed} --max-epochs 10000 --verbose \
+        --name-prefix ${experiment_name} --remove-existing-data
 done
