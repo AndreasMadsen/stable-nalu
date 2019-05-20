240f32e51fb2f109dff16392de21a82af354fb77
Andreas Madsen <amwebdk@gmail.com>
2019-05-20 21:09:28 +0200

    fix mnist csv metrics

--- lfs_batch_jobs/sequential_mnist_csv_metrics.sh
+++ lfs_batch_jobs/sequential_mnist_csv_metrics.sh
@@ -1,10 +1,10 @@
 
 job_name='sequential_mnist_csv_metrics'
-experiment_names=( 'sequential_mnist_sum_old' 'sequential_mnist_prod_old' )
+experiment_names=( 'sequential_mnist_sum' 'sequential_mnist_prod' )
 
 for experiment_name in "${experiment_names[@]}"
 do
-    bsub -q compute -n 8 -W 3:00 -J ${job_name} -o /work3/$USER/logs/${job_name}/ -e /work3/$USER/logs/${job_name}/ -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
+    bsub -q compute -n 16 -W 3:00 -J ${job_name} -o /work3/$USER/logs/${job_name}/ -e /work3/$USER/logs/${job_name}/ -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
         export/sequential_mnist.py \
         --tensorboard-dir /work3/$USER/tensorboard/${experiment_name}/ \
         --csv-out ./results/${experiment_name}.csv
