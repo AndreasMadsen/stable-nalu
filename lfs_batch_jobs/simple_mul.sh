a796ba77c453a65da1d6f23eb43da0c74794da70
Andreas Madsen <amwebdk@gmail.com>
2019-06-18 11:25:54 +0200

    add clipping and linear bias to function task static lfs batches

--- lfs_batch_jobs/simple_mul.sh
+++ lfs_batch_jobs/simple_mul.sh
@@ -20,6 +20,63 @@ do
     bsub -q compute -n 8 -W 0:20 -J ${experiment_name} -o /work3/$USER/logs/${experiment_name}/ -e /work3/$USER/logs/${experiment_name}/ -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
         experiments/simple_function_static.py \
         --operation mul --layer-type ReRegualizedLinearNAC --nac-mul mnac \
+        --nac-oob regualized \
+        --simple \
+        --seed ${seed} --max-iterations 200000 --verbose \
+        --name-prefix ${experiment_name} --remove-existing-data
+
+    bsub -q compute -n 8 -W 0:20 -J ${experiment_name} -o /work3/$USER/logs/${experiment_name}/ -e /work3/$USER/logs/${experiment_name}/ -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
+        experiments/simple_function_static.py \
+        --operation mul --layer-type ReRegualizedLinearNAC --nac-mul mnac \
+        --nac-oob regualized --mnac-epsilon 1e-5 \
+        --simple \
+        --seed ${seed} --max-iterations 200000 --verbose \
+        --name-prefix ${experiment_name} --remove-existing-data
+
+    bsub -q compute -n 8 -W 0:20 -J ${experiment_name} -o /work3/$USER/logs/${experiment_name}/ -e /work3/$USER/logs/${experiment_name}/ -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
+        experiments/simple_function_static.py \
+        --operation mul --layer-type ReRegualizedLinearNAC --nac-mul mnac \
+        --nac-oob regualized --regualizer-shape linear \
+        --simple \
+        --seed ${seed} --max-iterations 200000 --verbose \
+        --name-prefix ${experiment_name} --remove-existing-data
+
+    bsub -q compute -n 8 -W 0:20 -J ${experiment_name} -o /work3/$USER/logs/${experiment_name}/ -e /work3/$USER/logs/${experiment_name}/ -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
+        experiments/simple_function_static.py \
+        --operation mul --layer-type ReRegualizedLinearNAC --nac-mul mnac \
+        --nac-oob regualized --mnac-epsilon 1e-5 --regualizer-shape linear \
+        --simple \
+        --seed ${seed} --max-iterations 200000 --verbose \
+        --name-prefix ${experiment_name} --remove-existing-data
+
+    bsub -q compute -n 8 -W 0:20 -J ${experiment_name} -o /work3/$USER/logs/${experiment_name}/ -e /work3/$USER/logs/${experiment_name}/ -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
+        experiments/simple_function_static.py \
+        --operation mul --layer-type ReRegualizedLinearNAC --nac-mul mnac \
+        --nac-oob clip \
+        --simple \
+        --seed ${seed} --max-iterations 200000 --verbose \
+        --name-prefix ${experiment_name} --remove-existing-data
+
+    bsub -q compute -n 8 -W 0:20 -J ${experiment_name} -o /work3/$USER/logs/${experiment_name}/ -e /work3/$USER/logs/${experiment_name}/ -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
+        experiments/simple_function_static.py \
+        --operation mul --layer-type ReRegualizedLinearNAC --nac-mul mnac \
+        --nac-oob clip --mnac-epsilon 1e-5 \
+        --simple \
+        --seed ${seed} --max-iterations 200000 --verbose \
+        --name-prefix ${experiment_name} --remove-existing-data
+
+    bsub -q compute -n 8 -W 0:20 -J ${experiment_name} -o /work3/$USER/logs/${experiment_name}/ -e /work3/$USER/logs/${experiment_name}/ -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
+        experiments/simple_function_static.py \
+        --operation mul --layer-type ReRegualizedLinearNAC --nac-mul mnac \
+        --nac-oob clip --regualizer-shape linear \
+        --simple \
+        --seed ${seed} --max-iterations 200000 --verbose \
+        --name-prefix ${experiment_name} --remove-existing-data
+
+    bsub -q compute -n 8 -W 0:20 -J ${experiment_name} -o /work3/$USER/logs/${experiment_name}/ -e /work3/$USER/logs/${experiment_name}/ -R "span[hosts=1]" -R "rusage[mem=10GB]" ./python_lfs_job.sh \
+        experiments/simple_function_static.py \
+        --operation mul --layer-type ReRegualizedLinearNAC --nac-mul mnac \
+        --nac-oob clip --mnac-epsilon 1e-5 --regualizer-shape linear \
         --simple \
         --seed ${seed} --max-iterations 200000 --verbose \
         --name-prefix ${experiment_name} --remove-existing-data
