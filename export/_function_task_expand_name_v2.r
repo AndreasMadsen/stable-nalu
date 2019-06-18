d28956ba2d95303c30f5f4c627fdc81da89b33ed
Andreas Madsen <amwebdk@gmail.com>
2019-06-18 11:26:38 +0200

    add debug batches for clipping and linear bias

---
+++ export/_function_task_expand_name_v2.r
@@ -0,0 +1,123 @@
+
+library(latex2exp)
+
+model.full.to.short = c(
+  'linear'='Linear',
+  'relu'='ReLU',
+  'relu6'='ReLU6',
+  'nac'='$\\mathrm{NAC}_{+}$',
+  'nac-nac-n'='$\\mathrm{NAC}_{\\bullet}$',
+  'posnac-nac-n'='$\\mathrm{NAC}_{\\bullet}$, $\\mathbf{W} = \\sigma(\\mathbf{\\hat{W}})$',
+  'nalu'='NALU',
+  'reregualizedlinearnac'='NAU',
+  'reregualizedlinearnac-nac-m'='NMU',
+  'regualizedlinearnac-nac-m'='NMU, $\\mathbf{W} = \\mathbf{\\hat{W}}$',
+  'sillyreregualizedlinearnac-nac-m'='NMU, $\\mathbf{z} = \\mathbf{W} \\odot \\mathbf{x}$'
+)
+
+model.latex.to.exp = c(
+  'Linear'='Linear',
+  'ReLU'='ReLU',
+  'ReLU6'='ReLU6',
+  '$\\mathrm{NAC}_{+}$'=TeX('$\\mathrm{NAC}_{+}$'),
+  '$\\mathrm{NAC}_{\\bullet}$'=expression(paste("", "", plain(paste("NAC")), 
+                                           phantom()[{
+                                             paste("", symbol("\xb7"))
+                                           }], "")),
+  'NALU'='NALU',
+  'NAU'='NAU',
+  'NMU'='NMU',
+  'NMU, $\\mathbf{z} = \\mathbf{W} \\odot \\mathbf{x}$'='NMU, z = W · x'
+)
+
+model.to.exp = function(v) {
+  return(unname(revalue(v, model.latex.to.exp)))
+}
+
+operation.full.to.short = c(
+  'op-add'='$\\bm{+}$',
+  'op-sub'='$\\bm{-}$',
+  'op-mul'='$\\bm{\\times}$',
+  'op-div'='$\\bm{\\mathbin{/}}$',
+  'op-squared'='$z^2$',
+  'op-root'='$\\sqrt{z}$'
+)
+
+extract.by.split = function (name, index, default=NA) {
+  split = strsplit(as.character(name), '_')[[1]]
+  if (length(split) >= index) {
+    return(split[index])
+  } else {
+    return(default)
+  }
+}
+
+range.full.to.short = function (range) {
+  range = substring(range, 3)
+  
+  if (substring(range, 0, 1) == '[') {
+    return(paste0('U', gsub('\\]-\\[', '] ∪ U[', gsub(' ', '', range))))
+  } else {
+    return(paste0('U[', gsub('^,', '-', gsub('-', ',', range)), ']'))
+  }
+}
+
+regualizer.get.sparse = function (regualizer) {
+  split = strsplit(regualizer, '-')[[1]]
+  return(as.double(split[2]))
+}
+
+regualizer.get.oob = function (regualizer) {
+  split = strsplit(regualizer, '-')[[1]]
+  if (length(split) == 2) {
+    return(as.double(1))
+  } else {
+    return(as.double(split[3]))
+  }
+}
+
+dataset.get.part = function (dataset, index, simple.value) {
+  split = strsplit(dataset, '-')[[1]]
+  if (split[2] == 'simple') {
+    return(simple.value)
+  } else {
+    return(as.numeric(split[index + 1]))
+  }
+}
+
+expand.name = function (df) {
+  names = data.frame(name=unique(df$name))
+  
+  df.expand.name = names %>%
+    rowwise() %>%
+    mutate(
+      model=revalue(extract.by.split(name, 1), model.full.to.short, warn_missing=FALSE),
+      operation=revalue(extract.by.split(name, 2), operation.full.to.short, warn_missing=FALSE),
+      
+      oob.control = ifelse(substring(extract.by.split(name, 3), 5) == "r", "regualized", "clip"),
+      regualizer.shape = substring(extract.by.split(name, 4), 4),
+      epsilon.zero = as.numeric(substring(extract.by.split(name, 5), 5)),
+      
+      regualizer=regualizer.get.sparse(extract.by.split(name, 6)),
+      regualizer.oob=regualizer.get.oob(extract.by.split(name, 6)),
+      
+      interpolation.range=range.full.to.short(extract.by.split(name, 7)),
+      extrapolation.range=range.full.to.short(extract.by.split(name, 8)),
+
+      input.size=dataset.get.part(extract.by.split(name, 9), 1, 4),
+      subset.ratio=dataset.get.part(extract.by.split(name, 9), 2, NA),
+      overlap.ratio=dataset.get.part(extract.by.split(name, 9), 3, NA),
+
+      batch.size=as.integer(substring(extract.by.split(name, 10), 2)),
+      seed=as.integer(substring(extract.by.split(name, 11), 2)),
+      hidden.size=as.integer(substring(extract.by.split(name, 12, 'h2'), 2)),
+    )
+  
+  df.expand.name$name = as.factor(df.expand.name$name)
+  df.expand.name$operation = factor(df.expand.name$operation, c('$\\bm{\\times}$', '$\\bm{+}$', '$\\bm{-}$'))
+  df.expand.name$model = as.factor(df.expand.name$model)
+  df.expand.name$interpolation.range = as.factor(df.expand.name$interpolation.range)
+  df.expand.name$extrapolation.range = as.factor(df.expand.name$extrapolation.range)
+  
+  return(merge(df, df.expand.name))
+}
---
+++ export/function_task_static_debug.r
@@ -0,0 +1,115 @@
+rm(list = ls())
+setwd(dirname(parent.frame(2)$ofile))
+
+library(plyr)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(kableExtra)
+source('./_function_task_expand_name_v2.r')
+source('./_function_task_table.r')
+
+best.range = 100
+
+best.model.step.fn = function (errors) {
+  best.step = max(length(errors) - best.range, 0) + which.min(tail(errors, best.range))
+  if (length(best.step) == 0) {
+    return(length(errors))
+  } else {
+    return(best.step)
+  }
+}
+
+first.solved.step = function (steps, errors, epsilon) {
+  index = first(which(errors < epsilon))
+  if (is.na(index)) {
+    return(NA)
+  } else {
+    return(steps[index])
+  }
+}
+
+t.confidence.interval = function (alpha, vec) {
+  return(abs(qt((1 - alpha) / 2, length(vec) - 1)) * (sd(vec) / sqrt(length(vec))))
+}
+
+safe.median = function (vec) {
+  if (length(vec) == 0) {
+    return(NA)
+  } else {
+    return(median(vec))
+  }
+}
+
+eps = read_csv('../results/function_task_static_mse_expectation.csv') %>%
+  filter(simple == FALSE & parameter == 'default') %>%
+  mutate(
+    operation = revalue(paste0("op", substring(operation, 2)), operation.full.to.short),
+    epsilon = mse
+  ) %>%
+  select(operation, epsilon)
+
+dat = expand.name(read_csv('../results/function_task_static_debug.csv')) %>%
+  merge(eps)
+
+dat.last = dat %>%
+  group_by(name) %>%
+  #filter(n() == 201) %>%
+  summarise(
+    epsilon = last(epsilon),
+    best.model.step = best.model.step.fn(loss.valid.interpolation),
+    interpolation.last = loss.valid.interpolation[best.model.step],
+    extrapolation.last = loss.valid.extrapolation[best.model.step],
+    interpolation.step.solved = first.solved.step(step, loss.valid.interpolation, epsilon),
+    extrapolation.step.solved = first.solved.step(step, loss.valid.extrapolation, epsilon),
+    sparse.error.max = sparse.error.max[best.model.step],
+    sparse.error.mean = sparse.error.sum[best.model.step] / sparse.error.count[best.model.step],
+    solved = replace_na(loss.valid.extrapolation[best.model.step] < epsilon, FALSE),
+    model = last(model),
+    oob.control = last(oob.control),
+    regualizer.shape = last(regualizer.shape),
+    epsilon.zero = last(epsilon.zero),
+    operation = last(operation),
+    seed = last(seed),
+    size = n()
+  )
+
+dat.last.rate = dat.last %>%
+  group_by(model, operation, oob.control, regualizer.shape, epsilon.zero) %>%
+  summarise(
+    rate.interpolation = mean(interpolation.last < epsilon),
+    rate.extrapolation = mean(solved),
+    
+    median.interpolation.solved = safe.median(interpolation.step.solved[solved]),
+    mean.interpolation.solved = mean(interpolation.step.solved[solved]),
+    
+    median.extrapolation.solved = safe.median(extrapolation.step.solved[solved]),
+    mean.extrapolation.solved = mean(extrapolation.step.solved[solved]),
+    ci.extrapolation.solved = t.confidence.interval(0.95, extrapolation.step.solved[solved]),
+    
+    median.sparse.error.max = safe.median(sparse.error.max[solved]),
+    mean.sparse.error.max = mean(sparse.error.max[solved]),
+    ci.sparse.error.max = t.confidence.interval(0.95, sparse.error.max[solved]),
+    
+    mean.sparse.error.mean = mean(sparse.error.mean[solved]),
+    size = n() 
+  )
+
+print(dat.last.rate)
+# 
+# save.table(
+#   dat.last.rate %>% filter(
+#     (operation %in% c('$\\bm{+}$', '$\\bm{-}$') & model %in% c('Linear', 'NAU', '$\\mathrm{NAC}_{+}$', 'NALU')) |
+#     (operation %in% c('$\\bm{\\times}$') & model %in% c('Linear', 'NMU', '$\\mathrm{NAC}_{\\bullet}$', 'NALU'))
+#   ),
+#   "function-task-static-defaults",
+#   "Shows the success-rate for $\\mathcal{L}_{\\mathbf{W}_1, \\mathbf{W}_2} < \\mathcal{L}_{\\mathbf{W}_1^\\epsilon, \\mathbf{W}_2^*}$, at what global step the model converged at and the sparsity error for all weight matrices, with 95\\% confidence interval. Best result is highlighed without considering significance.",
+#   "../paper/results/function_task_static.tex"
+# )
+# 
+# save.table(
+#   dat.last.rate,
+#   "function-task-static-defaults-all",
+#   "Shows the success-rate for $\\mathcal{L}_{\\mathbf{W}_1, \\mathbf{W}_2} < \\mathcal{L}_{\\mathbf{W}_1^\\epsilon, \\mathbf{W}_2^*}$, at what global step the model converged at and the sparsity error for all weight matrices, with 95\\% confidence interval. Best result is highlighed without considering significance.",
+#   "../paper/results/function_task_static_all.tex"
+# )
