fix softmax transform, subset mnist digits, add parameters to mnist script

stable_nalu/dataset/sequential_mnist.py
@@ -42,7 +42,7 @@ DATA_DIR = path.join(THIS_DIR, 'data')
 class SequentialMnistDataset:
     def __init__(self, operation,
                  num_workers=1,
-                 remove_zero=False,
+                 mnist_digits=[0,1,2,3,4,5,6,7,8,9],
                  seed=None,
                  use_cuda=False):
         super().__init__()
@@ -51,7 +51,7 @@ class SequentialMnistDataset:
         self._num_workers = num_workers
         self._use_cuda = use_cuda
         self._rng = np.random.RandomState(seed)
-        self._remove_zero = remove_zero
+        self._mnist_digits = set(mnist_digits)
 
     def get_item_shape(self):
         if self._operation == OPERATIONS.sum:
@@ -79,7 +79,7 @@ class SequentialMnistDatasetFork(torch.utils.data.Dataset):
         self._operation = parent._operation
         self._num_workers = parent._num_workers
         self._use_cuda = parent._use_cuda
-        self._remove_zero = parent._remove_zero
+        self._mnist_digits = parent._mnist_digits
 
         self._subset = subset
         self._seq_length = seq_length
@@ -96,12 +96,9 @@ class SequentialMnistDatasetFork(torch.utils.data.Dataset):
                 torchvision.transforms.Normalize((0.1307,), (0.3081,))
             ])
         )
-        if self._remove_zero:
-            self._index_mapping = self._rng.permutation([
-                i for (i, (x, t)) in enumerate(self._dataset) if t != 0
-            ])
-        else:
-            self._index_mapping = self._rng.permutation(len(self._dataset))
+        self._index_mapping = self._rng.permutation([
+            i for (i, (x, t)) in enumerate(self._dataset) if t in self._mnist_digits
+        ])
 
     def __getitem__(self, index):
         mnist_images = []

stable_nalu/layer/generalized.py
@@ -172,19 +172,15 @@ class GeneralizedCell(ExtendedTorchModule):
             self.cell = PassThoughCell(input_size, hidden_size,
                                        **kwags)
         elif unit_name == 'GRU':
-            self.cell = torch.nn.GRUCell(input_size, hidden_size,
-                                          **kwags)
+            self.cell = torch.nn.GRUCell(input_size, hidden_size)
         elif unit_name == 'LSTM':
-            self.cell = torch.nn.LSTMCell(input_size, hidden_size,
-                                          **kwags)
+            self.cell = torch.nn.LSTMCell(input_size, hidden_size)
         elif unit_name == 'RNN-tanh':
             self.cell = torch.nn.RNNCell(input_size, hidden_size,
-                                         nonlinearity='tanh',
-                                         **kwags)
+                                         nonlinearity='tanh')
         elif unit_name == 'RNN-ReLU':
             self.cell = torch.nn.RNNCell(input_size, hidden_size,
-                                         nonlinearity='relu',
-                                         **kwags)
+                                         nonlinearity='relu')
         elif unit_name == 'RNN-linear':
             self.cell = BasicCell(input_size, hidden_size,
                                   activation='linear',

stable_nalu/network/regression_mnist.py
@@ -7,7 +7,9 @@ from ..layer import GeneralizedLayer, GeneralizedCell
 # reset_parameters method and changed final layer to have one output.
 
 class RegressionMnisNetwork(ExtendedTorchModule):
-    def __init__(self, softmax_transform=False, **kwargs):
+    def __init__(self,
+                 mnist_digits=[0,1,2,3,4,5,6,7,8,9],
+                 softmax_transform=False, **kwargs):
         super().__init__('cnn', **kwargs)
         self._softmax_transform = softmax_transform
 
@@ -15,8 +17,8 @@ class RegressionMnisNetwork(ExtendedTorchModule):
         self.conv2 = torch.nn.Conv2d(20, 50, 5, 1)
         self.fc1 = torch.nn.Linear(4*4*50, 500)
         if self._softmax_transform:
-            self.fc2 = torch.nn.Linear(500, 10)
-            self.fc3 = torch.nn.Linear(10, 1)
+            self.fc2 = torch.nn.Linear(500, num_mnist_classes)
+            self.register_buffer('fc3', torch.tensor(mnist_digits).reshape(1, -1))
         else:
             self.fc2 = torch.nn.Linear(500, 1)
 
@@ -37,6 +39,6 @@ class RegressionMnisNetwork(ExtendedTorchModule):
 
         if self._softmax_transform:
             x = torch.nn.functional.softmax(x, dim=-1)
-            x = self.fc3(x)
+            x = x * self.fc3
 
         return x